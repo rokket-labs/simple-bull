a193eed66580f6c3f81f018a56e4ee68b93d8c77
Robert <robert@orionx.com>
2020-06-15 22:36:49 -0400

    fixing queue and job onFail functions

--- src/queue/onFailed.js
+++ src/queue/onFailed.js
@@ -1,20 +1,19 @@
 import { errorMessage } from '../logger'
 
-export default function ({ queue, jobs, queueOnFail }) {
+export default function ({ queue, jobs, onFail }) {
   queue.on('failed', function (currentJob, err) {
-    console.log('simple bull failing')
     const findJobByName = ({ name }) => name === currentJob.name
     const firedJob = jobs.find(findJobByName)
 
-    if (queueOnFail) {
-      queueOnFail({ err, currentJob, errorMessage })
+    if (onFail) {
+      onFail({ err, currentJob, errorMessage })
     }
 
     if (firedJob.onFail) {
       firedJob.onFail({ err, currentJob, errorMessage })
     }
     // If they decide to handle fail event they have to return the message
-    if (queueOnFail || firedJob.onFail) return
+    if (onFail || firedJob.onFail) return
 
     return errorMessage(currentJob, err)
   })
--- src/redisConf.js
+++ src/redisConf.js
@@ -1,9 +1,9 @@
-const port = process.env.REDIS_PORT || 6970;
-const host = process.env.REDIS_HOST || 'localhost';
-const password = process.env.REDIS_PASSWORD || '';
-const stringUrl = process.env.REDIS_STRING_URL;
+const port = process.env.REDIS_PORT || 6970
+const host = process.env.REDIS_HOST || 'localhost'
+const password = process.env.REDIS_PASSWORD || ''
+const stringUrl = process.env.REDIS_STRING_URL
 
 export default function () {
-  if (stringUrl) return stringUrl;
-  return { redis: { port, host, password } };
+  if (stringUrl) return stringUrl
+  return { redis: { port, host, password } }
 }
