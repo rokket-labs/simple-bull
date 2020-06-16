f30e8ba848445201acebb286b61b99dcfb831c90
Robert <robert@orionx.com>
2020-06-16 12:10:39 -0400

    removing console log and fixing name convention

--- src/queue/onComplete.js
+++ src/queue/onComplete.js
@@ -1,13 +1,12 @@
 import { successMessage } from '../logger'
 
-export default function ({ queue, jobs, queueOnSuccess }) {
+export default function ({ queue, jobs, onSuccess }) {
   queue.on('completed', function (currentJob, result) {
-    console.log(result, 'this is the result')
     const findJobByName = ({ name }) => name === currentJob.name
     const firedJob = jobs.find(findJobByName)
 
-    if (queueOnSuccess) {
-      queueOnSuccess({ result, job: currentJob, successMessage })
+    if (onSuccess) {
+      onSuccess({ result, job: currentJob, successMessage })
     }
 
     if (firedJob.onSuccess) {
@@ -15,7 +14,7 @@ export default function ({ queue, jobs, queueOnSuccess }) {
     }
     currentJob.remove()
     // If they decide to handle success event they have to return the message
-    if (queueOnSuccess || firedJob.onSuccess) return
+    if (onSuccess || firedJob.onSuccess) return
 
     return successMessage(currentJob, result)
   })
